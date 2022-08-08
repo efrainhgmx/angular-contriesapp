b7f2ff89e591b3d3031f19ab12a089240c638da7
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-08 17:58:46 -0500

    table control

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,6 +12,7 @@ export class PorPaisComponent  {
 
   termino: string = 'Hola mundo';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService){}
 
@@ -20,8 +22,10 @@ export class PorPaisComponent  {
 
     this.paisService.buscarPais(this.termino).subscribe( (paises) => {
       console.log(paises);
+      this.paises = [...paises];
     }, (err) => {
       this.hayError = true;
+      this.paises = [];
     });
   }
 }
