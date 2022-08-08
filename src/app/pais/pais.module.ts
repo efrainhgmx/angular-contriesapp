5aeb2d193099f7965fa94c9590dd014afb896ad4
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-08 18:15:35 -0500

    table component

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -6,6 +6,7 @@ import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { PaisTablaComponent } from './components/pais-tabla/pais-tabla/pais-tabla.component';
 
 
 
@@ -14,7 +15,8 @@ import { RouterModule } from '@angular/router';
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisTablaComponent,
   ],
   exports: [
     PorCapitalComponent,
