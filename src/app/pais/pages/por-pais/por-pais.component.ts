523e7874c97a3e9e4f351e3e98488dda100eb32d
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-11 14:59:40 -0500

    seach by capital

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -19,11 +19,9 @@ export class PorPaisComponent  {
   buscar( termino: string) {
     this.hayError = false;
     this.termino = termino;
-    console.log(this.termino);
 
     this.paisService.buscarPais(this.termino).subscribe( (paises) => {
-      console.log(paises);
-      this.paises = [...paises];
+      this.paises = paises;
     }, (err) => {
       this.hayError = true;
       this.paises = [];
