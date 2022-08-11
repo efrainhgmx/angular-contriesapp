3ef43c71fe16c9d52fcf527db76b56a0417f30ba
Efrain Hernandez <efrainhg2050@gmail.com>
2022-08-11 15:17:36 -0500

    pais view

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -21,5 +21,10 @@ export class PaisService {
     const url = `${this.apiUrl}/capital/${termino}`;
     return this.http.get<Country[]>(url);
   }
+
+  paisPorCodigo( id: string ):Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
   
 }
